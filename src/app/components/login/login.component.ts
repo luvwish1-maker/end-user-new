5f525ee66c63cc00dfc588b15322431ac4a1826e
kuriakose <kuriakose00@gmail.com>
2025-10-15 12:42:04 +0530

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { finalize } from 'rxjs';
+import { AuthService } from '../../core/interceptor/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loading = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private modalService: NgbModal
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.loading = true;
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password).pipe(
+      finalize(() => this.loading = false)
+    ).subscribe({
+      next: (res: any) => {
+        this.close(res);
+      },
+      error: (err) => {
+        if (err.status === 404) {
+          this.signup(email, password);
+        } else {
+          console.log(err);
+        }
+      }
+    });
+  }
+
+  signup(email: string, password: string) {
+    this.authService.signup({ email, password }).subscribe({
+      next: () => alert('Success'),
+      error: () => alert('Error')
+    });
+  }
+
+  close(res?: any) {
+    if (res) {
+      this.modalService.dismissAll(res)
+    } else {
+      this.modalService.dismissAll()
+    }
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/interceptor/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../../components/login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +13,21 @@ import { AuthService } from '../../core/interceptor/auth.service';
 export class HeaderComponent implements OnInit {
   isLoggedIn = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
     this.authService.isLoggedIn$().subscribe(status => {
       this.isLoggedIn = status;
     });
+  }
+
+  openLogin() {
+    const buttonElement = document.activeElement as HTMLElement;
+    buttonElement.blur();
+
+    this.modalService.open(LoginComponent, { size: 'md', centered: true, backdrop: 'static' });
   }
 }
