be213eb52e3172346cb5c710aadc9d282b992210
kuriakose <kuriakose00@gmail.com>
2025-10-15 10:26:12 +0530

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListComponent } from './components/list/list.component';
+import { DetailsComponent } from './components/details/details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'list', component: ListComponent },
+    { path: 'details/:id', component: DetailsComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'wishlist', component: WishlistComponent },
+    { path: 'summary', component: SummaryComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '' }
+];
